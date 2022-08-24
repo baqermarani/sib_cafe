5ce05e71ce68d211718001d0f6b79b0fb4d64095
baqermarani <baqermarani@gmail.com>
2022-08-25 03:18:55 +0430

    postgresql configure in settings.py

--- config/settings.py
+++ config/settings.py
@@ -73,7 +73,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
