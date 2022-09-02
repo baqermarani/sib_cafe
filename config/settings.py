35e749f548b617821e0510ab203bdffb2c61b6ad
baqermarani <baqermarani@gmail.com>
2022-09-03 02:19:22 +0430

    config new apps

--- config/settings.py
+++ config/settings.py
@@ -38,10 +38,13 @@ INSTALLED_APPS = [
 
     # Local APP
     'accounts.apps.AccountsConfig',
+    'core.apps.CoreConfig',
+    'orders.apps.OrdersConfig',
 
     # Third Party App
     'crispy_forms',
     "crispy_bootstrap5",
+    'sorl.thumbnail',
 
 ]
 
@@ -127,6 +130,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
